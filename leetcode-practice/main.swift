9fec7834507416307252bbbdb8dcd38b3d0bc4b7
Gred <jihong.park.dev@gmail.com>
2020-01-09 18:39:26 +0900

    Initial Commit

---
+++ leetcode-practice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  leetcode-practice
+//
+//  Created by 박지홍 on 2020/01/09.
+//  Copyright © 2020 gred. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
